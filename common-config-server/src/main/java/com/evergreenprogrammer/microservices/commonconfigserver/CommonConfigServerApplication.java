cbb4b9d70be2a094dfa5bfd6816db1c9300cc537
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-10-22 16:20:55 +0530

    common-config-server

--- common-config-server/src/main/java/com/evergreenprogrammer/microservices/commonconfigserver/CommonConfigServerApplication.java
+++ common-config-server/src/main/java/com/evergreenprogrammer/microservices/commonconfigserver/CommonConfigServerApplication.java
@@ -2,12 +2,10 @@ package com.evergreenprogrammer.microservices.commonconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient(autoRegister=true)
 public class CommonConfigServerApplication {
 
 	public static void main(String[] args) {
